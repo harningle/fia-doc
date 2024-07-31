4c3a152a78eac4d97d06fcdced95fe747306fb41
Xuanyi WANG <wangxuanyi10@gmail.com>
2024-07-31 03:43:19 +0100

    parse all quali PDFs to one combined data file

---
+++ models/quali_lap.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from datetime import timedelta
+
+from pydantic import BaseModel, PositiveInt
+
+from models.foreign_key import SessionEntry
+
+
+class Lap(BaseModel):
+    number: PositiveInt
+    time: timedelta
+    is_deleted: bool
+    is_fastest_lap: bool
+
+
+class LapData(BaseModel):
+    object_type: str = 'lap'
+    foreign_keys: SessionEntry
+    objects: list[Lap]
