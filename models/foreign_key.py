52002eb673263bdb885cb6d9f43c7d58a9ee140a
Xuanyi WANG <wangxuanyi10@gmail.com>
2024-09-12 01:58:13 +0100

    ren. `type` --> `session` in data models
    * `type` is session type and now we call it `session`. It now takes the values race, Q1, Q2, ...
    * also explicitly disallow additional fields in all data models

--- models/foreign_key.py
+++ models/foreign_key.py
@@ -2,16 +2,20 @@
 """Frequently used foreign key models for the data objects"""
 from typing import Literal
 
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, ConfigDict, PositiveInt
 
 
 class SessionEntry(BaseModel):
     year: PositiveInt
     round: PositiveInt
-    type: str = Literal['R', 'Q', 'SR']  # Race, Quali, Sprint. TODO: enough?
+    session: str = Literal['R', 'Q1', 'Q2', 'Q3', 'SR', 'SQ1', 'SQ2', 'SQ3', 'FP1', 'FP2', 'FP3']
     car_number: PositiveInt
+
+    model_config = ConfigDict(extra='forbid')
 
 
 class Round(BaseModel):
     year: PositiveInt
     round: PositiveInt
+
+    model_config = ConfigDict(extra='forbid')
--- models/lap.py
+++ models/lap.py
@@ -1,29 +1,46 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta
 
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, ConfigDict, PositiveInt
 
 from models.foreign_key import SessionEntry
 
 
 class Lap(BaseModel):
-    lap_number: PositiveInt
+    number: PositiveInt
     position: PositiveInt
     time: timedelta
+
+    model_config = ConfigDict(extra='forbid')
+
+
+class QualiLap(BaseModel):
+    number: PositiveInt
+    time: timedelta
+    is_deleted: bool
+    is_entry_fastest_lap: bool
+
+    model_config = ConfigDict(extra='forbid')
 
 
 class FastestLap(BaseModel):
     lap_number: PositiveInt
     fastest_lap_rank: PositiveInt
 
+    model_config = ConfigDict(extra='forbid')
+
 
 class LapData(BaseModel):
     object_type: str = 'lap'
     foreign_keys: SessionEntry
-    objects: list[Lap]
+    objects: list[Lap | QualiLap]
+
+    model_config = ConfigDict(extra='forbid')
 
 
 class FastestLapData(BaseModel):
     object_type: str = 'fastest_lap'
     foreign_keys: SessionEntry
     objects: FastestLap
+
+    model_config = ConfigDict(extra='forbid')
