737d4ba4707e82330e67e8c3000ff627acfb3029
Xuanyi WANG <wangxy@wangx.onmicrosoft.com>
2025-02-26 00:57:34 +0100

    map driver and team names to IDs in jolpica DB in data dumps

--- fiadoc/models/foreign_key.py
+++ fiadoc/models/foreign_key.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """Frequently used foreign key models for the data objects"""
 from typing import Literal
+from typing_extensions import Self
 
-from pydantic import BaseModel, ConfigDict, PositiveInt, field_validator
+from pydantic import BaseModel, ConfigDict, PositiveInt, field_validator, model_validator
+
+from .._constants import DRIVERS, TEAMS
 
 
 class SessionEntry(BaseModel):
@@ -31,8 +34,36 @@ class SessionEntry(BaseModel):
 class RoundEntry(BaseModel):
     year: PositiveInt
     round: PositiveInt
-    driver_name: str
-    team_name: str
+    driver_reference: str
+    team_reference: str
+
+    @model_validator(mode='before')
+    def get_team_reference(self) -> Self:
+        if self['year'] in TEAMS:
+            if self['team_reference'] in TEAMS[self['year']]:
+                self['team_reference'] = TEAMS[self['year']][self['team_reference']]
+                return self
+            else:
+                raise ValueError(f"team {self['team_reference']} not found in year "
+                                 f"{self['year']}'s team name mapping. Available teams: "
+                                 f"{TEAMS[self['year']].keys()}")
+        else:
+            raise ValueError(f'year {self['year']} not found in team name mapping. Available '
+                             f'years: {TEAMS.keys()}')
+
+    @model_validator(mode='before')
+    def get_driver_name(self) -> Self:
+        if self['year'] in DRIVERS:
+            if self['driver_reference'] in DRIVERS[self['year']]:
+                self['driver_reference'] = DRIVERS[self['year']][self['driver_reference']]
+                return self
+            else:
+                raise ValueError(f"driver {self['driver_reference']} not found in year "
+                                 f"{self['year']}'s driver name mapping. Available drivers: "
+                                 f"{DRIVERS[self['year']].keys()}")
+        else:
+            raise ValueError(f'year {self['year']} not found in driver name mapping. Available '
+                             f'years: {DRIVERS.keys()}')
 
     model_config = ConfigDict(extra='forbid')
 
