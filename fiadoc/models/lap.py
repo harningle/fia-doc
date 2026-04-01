b4ee7097cf59320ba92919443436673a3eeb383c
Xuanyi WANG <wangxy@wangx.onmicrosoft.com>
2026-04-01 23:59:07 +0200

    fix: convert `np.nan` to `None` in pydantic models

--- fiadoc/models/lap.py
+++ fiadoc/models/lap.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 from jolpica.schemas import data_import
-from pydantic import ConfigDict
+from pydantic import ConfigDict, field_validator
 
 from .foreign_key import SessionEntryForeignKeys
 
@@ -9,6 +10,13 @@ class LapObject(data_import.LapObject):
     time: dict[str, str | int]  # TODO: what's the time here??
 
     model_config = ConfigDict(extra='forbid')
+
+    @field_validator('number', mode='before')
+    @classmethod
+    def _nan_to_none(cls, v):
+        if isinstance(v, float) and np.isnan(v):
+            return None
+        return v
 
 
 class LapImport(data_import.LapImport):
